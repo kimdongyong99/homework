1e8d12c589c29e716c7319854b0f85483c36ced7
kimdongyong99 <homeland007@naver.com>
2024-07-03 15:48:26 +0900

    up down.py

--- up down.py
+++ up down.py
@@ -1,38 +1,45 @@
 import random
 
+options = ['가위', '바위', '보']
 
-def up_down_game():
+
+def get_player_choice():
     while True:
-        print("=== Up Down 게임을 시작합니다 ===")
-        number = random.randint(1, 100)
-        attempts = 0
-
-        while True:
-            try:
-                guess = int(input("1에서 100 사이의 숫자를 입력하세요: "))
-                attempts += 1
-
-                if guess < 1 or guess > 100:
-                    print("잘못된 입력입니다. 1에서 100 사이의 숫자를 입력하세요.")
-                    continue
-
-                if guess < number:
-                    print("Up! 더 큰 숫자를 입력하세요.")
-                elif guess > number:
-                    print("Down! 더 작은 숫자를 입력하세요.")
-                else:
-                    print(f"정답입니다! {attempts}번 만에 맞추셨습니다.")
-                    break
-
-            except ValueError:
-                print("잘못된 입력입니다. 숫자를 입력하세요.")
-                continue
-
-        play_again = input("다시 하시겠습니까? (yes/no): ").lower()
-        if play_again != 'yes':
-            print("게임을 종료합니다.")
-            break
+        player_choice = input('가위, 바위, 보 중 하나를 선택하세요: ')
+        if player_choice in ['가위', '바위', '보']:
+            return player_choice
+        else:
+            print('잘못된 입력입니다. 다시 입력하세요.')
 
 
-if __name__ == "__main__":
-    up_down_game()
+win = 0
+draw = 0
+lose = 0
+
+
+while True:
+    player_choice = get_player_choice()
+
+    computer_choice = random.choice(options)
+    print(computer_choice)
+
+    if (
+        (player_choice == '가위' and computer_choice == '보')
+            or (player_choice == '바위' and computer_choice == '가위')
+            or (player_choice == '보' and computer_choice == '바위')):
+        print('이겼음')
+        win += 1
+    elif player_choice == computer_choice:
+        print('비겼음')
+        draw += 1
+    else:
+        print('졌음')
+        lose += 1
+
+    retry = input('다시하시겠습니까? (Y/N)').lower()
+    if retry == 'y':
+        continue
+    elif retry == 'n':
+        break
+
+print(f"승리 : {win}회 / 비김 : {draw}회 / 패배 : {lose}회")
